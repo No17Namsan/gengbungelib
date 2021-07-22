6478841bbe5fd22ec1b13868a0b9a4bc8c341ca1
No17Namsan <2012nexennst@gmail.com>
2021-07-22 15:27:41 +0900

    Add files via upload

---
+++ GenGLoLPlayer.py
@@ -0,0 +1,27 @@
+class GenGLoLPlayer:
+    def __init__(self):
+        self.meme=[]
+
+
+Rascal = GenGLoLPlayer()
+Rascal.meme.append('방패로 맞아보실?')
+
+
+Clid=GenGLoLPlayer()
+Clid.meme.append('Bdd 아닙니다.')
+
+Flawless=GenGLoLPlayer()
+Flawless.meme.append('추가예정')
+
+Bdd=GenGLoLPlayer()
+Bdd.meme.append('Clid 아닙니다.')
+
+Karis=GenGLoLPlayer()
+Karis.meme.append('성골유스라고 들어보셨습니까?')
+
+Ruler=GenGLoLPlayer()
+Ruler.meme.append('펜타펜타펜타펜타 야 펜타펜타 야 이 개새')
+Ruler.meme.append('댕댕좌')
+
+Life=GenGLoLPlayer()
+Life.meme.append('꼰대 아닙니다.')
